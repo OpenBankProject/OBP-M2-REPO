76d84654aa2cda7a619cbe2ac57c05031f0dd43d
Petar Bozin <petar.bozin@md-softwaredesign.de>
2016-11-15 17:12:21 +0100

    Initial commit. obp-ri-transport and obp-ri-kafka, 2016.9-ALPHA11-SNAPSHOT, next branch

---
+++ install2repo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+version=2016.9-ALPHA11-SNAPSHOT
+user=$(whoami)
+
+mvn install:install-file \
+ -DgroupId=com.tesobe.obp \
+ -DartifactId=obp-ri-transport \
+ -Dversion=${version} \
+ -Dpackaging=jar \
+ -Dfile=/home/${user}/.m2/repository/com/tesobe/obp/obp-ri-transport/${version}/obp-ri-transport-${version}.jar \
+ -DlocalRepositoryPath=.
+
+mvn install:install-file \
+ -DgroupId=com.tesobe.obp \
+ -DartifactId=obp-ri-kafka \
+ -Dversion=${version} \
+ -Dpackaging=jar \
+ -Dfile=/home/${user}/.m2/repository/com/tesobe/obp/obp-ri-kafka/${version}/obp-ri-kafka-${version}.jar \
+ -DlocalRepositoryPath=.

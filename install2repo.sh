5d962234e49b05c0ca84528b1238e0d527eff400
Petar Bozin <petar.bozin@md-softwaredesign.de>
2016-11-16 12:34:54 +0100

    Major change - now using deploy-file instead of install-file

--- install2repo.sh
+++ install2repo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script needs to be executed from the same directory:
+# ./install2repo.sh
+
 # Set this to version of locally installed 
 # package in default ~/.m2/ directory
 version=2016.9-ALPHA11-SNAPSHOT
@@ -7,31 +10,24 @@ version=2016.9-ALPHA11-SNAPSHOT
 
 
 user=$(whoami)
-mvn install:install-file \
+temp_repo=/tmp/obp-temp-repo-${user}/com/tesobe
+rm -rf /tmp/obp-temp-repo-${user}
+mkdir -p ${temp_repo}
+rsync -a /home/${user}/.m2/repository/com/tesobe/ ${temp_repo}/ 
+
+mvn deploy:deploy-file \
  -DgroupId=com.tesobe.obp \
  -DartifactId=obp-ri-transport \
  -Dversion=${version} \
- -Dpackaging=jar \
- -Dfile=/home/${user}/.m2/repository/com/tesobe/obp/obp-ri-transport/${version}/obp-ri-transport-${version}.jar \
- -DlocalRepositoryPath=.
-mvn install:install-file \
- -DgroupId=com.tesobe.obp \
- -DartifactId=obp-ri-transport \
- -Dversion=${version} \
- -Dpackaging=pom \
- -Dfile=/home/${user}/.m2/repository/com/tesobe/obp/obp-ri-transport/${version}/obp-ri-transport-${version}.pom \
- -DlocalRepositoryPath=.
-mvn install:install-file \
+ -DrepositoryId=git-OpenBankProject \
+ -Dfile=${temp_repo}/obp/obp-ri-transport/${version}/obp-ri-transport-${version}.jar \
+ -Durl=file://$(pwd)
+
+mvn deploy:deploy-file \
  -DgroupId=com.tesobe.obp \
  -DartifactId=obp-ri-kafka \
  -Dversion=${version} \
- -Dpackaging=jar \
- -Dfile=/home/${user}/.m2/repository/com/tesobe/obp/obp-ri-kafka/${version}/obp-ri-kafka-${version}.jar \
- -DlocalRepositoryPath=.
-mvn install:install-file \
- -DgroupId=com.tesobe.obp \
- -DartifactId=obp-ri-kafka \
- -Dversion=${version} \
- -Dpackaging=pom \
- -Dfile=/home/${user}/.m2/repository/com/tesobe/obp/obp-ri-kafka/${version}/obp-ri-kafka-${version}.pom \
- -DlocalRepositoryPath=.
+ -DrepositoryId=git-OpenBankProject \
+ -Dfile=${temp_repo}/obp/obp-ri-kafka/${version}/obp-ri-kafka-${version}.jar \
+ -Durl=file://$(pwd)
+
